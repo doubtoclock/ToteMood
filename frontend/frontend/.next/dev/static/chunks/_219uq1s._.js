c3f252cecee9fbec353eb86965a9b509e09e8346
Chinmaystar <chinmaysabharwal123@gmail.com>
2026-08-20 20:34:09 +0530

    fix: remove build artifacts, add .gitignore entries

--- frontend/frontend/.next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_app_1-0bt8n._.css","static/chunks/node_modules_next_1tuyk14._.js","static/chunks/node_modules_motion-dom_dist_es_1v907r9._.js","static/chunks/node_modules_framer-motion_dist_es_1s5ihya._.js","static/chunks/node_modules_1lm66y4._.js","static/chunks/src_007qdb3._.js","static/chunks/_0iofqya._.js","static/chunks/src_07ik9pf._.js","static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_0q79z23._.js","static/chunks/node_modules_three_build_three_core_18d0oq6.js","static/chunks/node_modules_three_build_three_module_03rsyo6.js","static/chunks/node_modules_three_build_three_module_1_wgwg8.js","static/chunks/node_modules_@react-three_fiber_dist_1ev1hxs._.js","static/chunks/node_modules_three-stdlib_1wlrcur._.js","static/chunks/node_modules_148emk7._.js"],
-    source: "entry"
-});
